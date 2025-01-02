a5ec52ed6048669f84b9657e47e6dec4fb423423
Putrajaya - PC25 <putrajaya-pc25@aiuit.onmicrosoft.com>
2025-01-02 17:28:44 +0800

    something

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -42,6 +42,8 @@ class _DashboardPageState extends State<DashboardPage> {
     fetchDashboardData();
     _startAutoRefresh();
     sendPostRequest(fcmToken:fcmToken ,mobileToken: widget.mobile_token);
+    requestNotificationPermission(); // Request notification permissions here
+    
   }
 
   @override
@@ -68,6 +70,26 @@ class _DashboardPageState extends State<DashboardPage> {
     }
   }
 
+Future<void> requestNotificationPermission() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print('User granted permission');
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    print('User granted provisional permission');
+  } else {
+    print('User declined or has not accepted permission');
+  }
+}
   Future<void> sendPostRequest({
     required String mobileToken,
     required String? fcmToken,
